bee9a15898dc8141fdca4738f7495904e5fe424b
loveneesh Chaudhary <lucky7a36@gmail.com>
2026-05-23 15:20:41 +0530

    correct the code

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file here
-// require("dotenv").config();
+require("dotenv").config();
 
 
-mongoosex
+mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB error:", error));
@@ -46,11 +46,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend Running");
-});
-
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
